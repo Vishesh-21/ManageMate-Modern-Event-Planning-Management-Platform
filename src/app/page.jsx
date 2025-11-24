e132436d4419d8aef430135c2406d93a9086587a
Vishu Verma <visheshbaund@gmail.com>
2025-11-24 16:15:45 +0530

    Adding hero section for the application.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="md:mt-0 mt-10">
+      <section className="pb-16 relative overflow-hidden">
+        <div className="max-w-7xl mx-auto grid lg:grid-cols-2 gap-4 items-center relative z-10">
+          {/* left  */}
+          <div className="text-center sm:text-left">
+            <span className="text-gray-500 font-light tracking-wide mb-6">
+              ManageMate<span className="text-purple-400">*</span>
+            </span>
+            <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-6 leading-[0.95] tracking-wider">
+              Smarter Tools <br />
+              for Seamless
+              <br />
+              <span className="bg-linear-to-r from-blue-400 via-purple-400 to-orange-400 bg-clip-text text-transparent">
+                Planning
+              </span>
+            </h1>
+            <p className="text-medium sm:text-medium text-gray-400 mb-8 max-w-lg font-light">
+              ManageMate helps you plan and run events with speed and precision, bringing tasks, schedules, and details into one smooth, stress-free workspace.
+            </p>
 
-export default HomePage
+            <Link href={"/explore"}>
+              <Button size="xl" className={"rounded-full cursor-pointer"}>
+                Get Started
+              </Button>
+            </Link>
+          </div>
+
+          {/* right */}
+          <div>
+            <Image
+              src={"/hero-section-image.png"}
+              alt="hero image"
+              width={300}
+              height={300}
+              className="w-[100%] h-auto scale-[1.1]"
+              priority
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
