7091b21d86c92a0ec1804ef011a1cf2d860b3f5f
Vishu Verma <visheshbaund@gmail.com>
2025-11-24 10:20:28 +0530

    Configure database for application ( Convex ).

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Inter } from "@next/font/google";
 import Header from "@/components/Header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 export const metadata = {
   title: "Manage Mate",
@@ -22,27 +23,29 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          {/* header  */}
-          <Header />
+          <ConvexClientProvider>
+            {/* header  */}
+            <Header />
 
-          <main className="min-h-screen relative container mx-auto pt-16 md:pt-24">
-            {/* glow effect  */}
-            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+            <main className="min-h-screen relative container mx-auto pt-16 md:pt-24">
+              {/* glow effect  */}
+              <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
 
-              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
-            </div>
-
-            <div className="relative z-10 min-h-[90vh]">{children}</div>
-
-            {/* footer  */}
-            <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
-              <div className="text-sm text-gray-400">
-                &copy; {new Date().getFullYear()} Manage Mate Inc. | All rights
-                reserved
+                <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
               </div>
-            </footer>
-          </main>
+
+              <div className="relative z-10 min-h-[90vh]">{children}</div>
+
+              {/* footer  */}
+              <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto">
+                <div className="text-sm text-gray-400 text-center">
+                  &copy; {new Date().getFullYear()} Manage Mate Inc. | All
+                  rights reserved
+                </div>
+              </footer>
+            </main>
+          </ConvexClientProvider>
         </ThemeProvider>
       </body>
     </html>
